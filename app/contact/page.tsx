8437b859e541d46296abde255ea83ae91f2deafc
Maxwell Braun <maxbraun175@gmail.com>
2024-09-25 19:51:02 -0400

    add email to contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,19 @@
+import Link from "next/link";
+
 export default function Contact() {
   return (
     <div>
       <h1 className="text-4xl font-bold mb-2">Contact Me</h1>
       <p>
         If you have questions, requests, want to report a bug, or anything else,
-        please send me an email at [email].
+        please send me an email at{" "}
+        <a
+          className="text-purple-400"
+          href="mailto:info@hypixelguildfinder.com"
+        >
+          info@hypixelguildfinder.com
+        </a>
+        .
       </p>
     </div>
   );
