06c773faa583d11045592f99a7b4331c171cd75b
Maxwell Braun <maxbraun175@gmail.com>
2024-09-29 20:56:54 -0400

    Restyle auth form messages

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -5,19 +5,21 @@ export type Message =
 
 export function FormMessage({ message }: { message: Message }) {
   return (
-    <div className="flex flex-col gap-2 w-full max-w-md text-sm">
+    <div className="flex flex-col gap-2 w-full max-w-md text-sm mt-2">
       {"success" in message && (
-        <div className="text-foreground border-l-2 border-foreground px-4">
+        <div className="text-foreground border-l-4 border-purple-500 bg-neutral-800 p-2 px-4">
           {message.success}
         </div>
       )}
       {"error" in message && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
+        <div className="text-destructive-foreground border-l-4 bg-neutral-800 p-2 border-red-500 px-4">
           {message.error}
         </div>
       )}
       {"message" in message && (
-        <div className="text-foreground border-l-2 px-4">{message.message}</div>
+        <div className="text-foreground border-l-4 border-white bg-neutral-800 p-2 px-4">
+          {message.message}
+        </div>
       )}
     </div>
   );
