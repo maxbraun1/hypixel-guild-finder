6afd563c4e432d06f3955d46d9ba4020c18982f3
Maxwell Braun <maxbraun175@gmail.com>
2025-08-03 09:35:34 -0400

    Add icons and discord link to menu

--- components/header-menu.tsx
+++ components/header-menu.tsx
@@ -7,7 +7,16 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Menu } from "lucide-react";
+import {
+  CircleUser,
+  House,
+  LogOut,
+  Mail,
+  Menu,
+  MessagesSquare,
+  Settings,
+  User,
+} from "lucide-react";
 import Link from "next/link";
 import { signOutAction } from "@/app/(app-wrapper)/actions/auth-actions";
 import { Button } from "./ui/button";
@@ -37,17 +46,27 @@ export default function HeaderMenu({
           </div>
         )}
       </DropdownMenuTrigger>
-      <DropdownMenuContent>
+      <DropdownMenuContent className="min-w-[150px]">
         <DropdownMenuItem asChild className="cursor-pointer">
-          <Link href="/">Home</Link>
+          <Link href="/" className="flex gap-1.5 items-center">
+            <House size={15} />
+            Home
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuItem asChild className="cursor-pointer">
-          <Link href="/account">My Account</Link>
+          <Link href="/account" className="flex gap-1.5 items-center">
+            <CircleUser size={15} />
+            My Account
+          </Link>
         </DropdownMenuItem>
         {guild && (
           <>
             <DropdownMenuItem asChild className="cursor-pointer">
-              <Link href="/requests/incoming" className="flex gap-2">
+              <Link
+                href="/requests/incoming"
+                className="flex gap-1.5 items-center"
+              >
+                <Mail size={15} />
                 Requests
                 {request_count > 0 && (
                   <div className="bg-red-500 rounded-full text-white text-xs px-2">
@@ -57,17 +76,34 @@ export default function HeaderMenu({
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild className="cursor-pointer">
-              <Link href="/account/guild-settings">Guild Settings</Link>
+              <Link
+                href="/account/guild-settings"
+                className="flex gap-1.5 items-center"
+              >
+                <Settings size={15} />
+                Guild Settings
+              </Link>
             </DropdownMenuItem>
           </>
         )}
+        <DropdownMenuItem asChild className="cursor-pointer">
+          <Link
+            href="https://discord.gg/4EskQNm3g4"
+            target="_blank"
+            className="flex gap-1.5 items-center"
+          >
+            <MessagesSquare size={15} />
+            Join Discord
+          </Link>
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
         <form action={signOutAction}>
           <Button
-            className="h-auto py-1.5 px-3 w-full"
+            className="h-auto py-1.5 px-3 w-full flex gap-1.5 border-none rounded-sm justify-start"
             type="submit"
             variant={"outline"}
           >
+            <LogOut size={15} />
             Sign out
           </Button>
         </form>
